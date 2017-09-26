cd245548d1589c86ba8d54f40069d65fa53872fe
vikkymahdrey <vikky.softengi@gmail.com>
2017-09-26 16:51:43 +0530

    push changes
    push changes

--- AtApplication/src/main/java/com/team/atapp/controller/LoginController.java
+++ AtApplication/src/main/java/com/team/atapp/controller/LoginController.java
@@ -38,7 +38,7 @@ public class LoginController {
 	
 	@RequestMapping(value= {"/onSubmitlogin"}, method=RequestMethod.POST)
 	public ModelAndView loginUser(HttpServletRequest request, HttpSession session, HttpServletResponse response,RedirectAttributes redirectAttributes) throws Exception{
-		
+		logger.debug("in /onSubmitlogin");
 		String username = request.getParameter("uname") == null ? "" : request
 				.getParameter("uname");
 		String password = request.getParameter("pass") == null ? "" : request
